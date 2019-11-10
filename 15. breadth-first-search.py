7239d63b59c892b8c6c724a0f53c2dc4f3f0df9f
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-11-10 15:55:57 -0500

    Breadth First Search exercise

---
+++ 15. breadth-first-search.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        queue = [self]
+        while len(queue) > 0:
+            node = queue[-1]
+            array.append(node.name)
+            del queue[-1]
+            for child in node.children:
+                queue.insert(0, child)
+
+        return array
+
+
+if __name__ == '__main__':
+    test1 = Node('A').addChild('B').addChild('C').addChild('D')
+    test1.children[0].addChild('E').addChild('F')
+    test1.children[2].addChild('G').addChild('H')
+    test1.children[0].children[1].addChild('I').addChild('J')
+    test1.children[2].children[0].addChild('K')
+
+    print(test1.breadthFirstSearch([]))
